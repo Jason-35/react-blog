0b4bde9182601b7f6eeb2c10ae5f6d5384c20c5e
Jason Wu <jsonwu34@gmail.com>
2023-08-18 14:07:52 -0400

    finish styling viewpost

--- frontend/src/routes/ViewPost.tsx
+++ frontend/src/routes/ViewPost.tsx
@@ -2,6 +2,7 @@ import { Form, useLoaderData } from "react-router-dom"
 import { commentObject, postObject } from "../dto/post.dto"
 import { Button, TextField } from "@mui/material"
 import { useState } from "react"
+import "../styles/ViewPost.css"
 
 export default function ViewPost(){
     const { postData, commentData } = useLoaderData() as { postData: postObject, commentData: commentObject[] }
@@ -14,20 +15,20 @@ export default function ViewPost(){
     }
 
     return (
-        <div style={{color: "black"}}>
+        <div className="view-post-container">
             <div className="title">
                 <h1>{postData.title}</h1>
             </div>
             <div className="author">
-                <div>{postData.user.username}</div>
+                <div>By {postData.user.username}</div>
                 <div>{postData.createdAt}</div>
             </div>
             <div className="content">
-                <pre>{postData.content}</pre>
+                <pre className="content-text">{postData.content}</pre>
             </div>
             <div className="comment-text">
-                <Form method="post" onSubmit={handleSubmit}>
-                    <div>
+                <Form method="post" className="comment-form" onSubmit={handleSubmit}>
+                    <div className="text-div">
                         <TextField
                             id="filled-multiline-flexible"
                             multiline
@@ -36,19 +37,17 @@ export default function ViewPost(){
                             value={textValue}
                             name="comment-field"
                             onChange={(e) => setTextValue(e.target.value)}
+                            className="comment-text-field"
                         />
-                    </div>
-                    <div>
                         <Button variant="contained" type="submit">Comment</Button>
                     </div>
                 </Form>
             </div>
             <div className="comments">
                 {commentData.map((item : commentObject) => (
-                    <div key={item.id}>
+                    <div className="comment-field" key={item.id}>
                         <div>{item.comment}</div>
                         <div>by {item.user.username}</div>
-                        <div>{item.createdAt}</div>
                     </div>
             ))}
             </div>
